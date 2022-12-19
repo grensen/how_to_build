d6ea3d299dd576a7b8dfb4b26c9bd1e5a1a1b8ab
Thorsten Kleppe <53048236+grensen@users.noreply.github.com>
2022-12-19 11:11:56 +0100

    Update how_to_build_demo.cs

--- how_to_build_demo.cs
+++ how_to_build_demo.cs
@@ -1,8 +1,8 @@
-// https://github.com/grensen/multi-core
-System.Action<string> print = System.Console.WriteLine;
+// https://github.com/grensen/how_to_build/
 #if DEBUG
-System.Console.WriteLine("Debug mode is on, switch to Release mode");
+    System.Console.WriteLine("Debug mode is on, switch to Release mode");
 #endif 
+System.Action<string> print = System.Console.WriteLine;
 
 print("\nBegin how to build neural networks demo\n");
 
@@ -10,20 +10,20 @@ print("\nBegin how to build neural networks demo\n");
 AutoData d = new(@"C:\mnist\");
 
 // define neural network 
-int[] network = { 784, 100, 100, 10 };
-var LEARNINGRATE = 0.0005f;
-var MOMENTUM = 0.67f;
-var EPOCHS = 50;
-var BATCHSIZE = 800;
-var FACTOR = 0.99f;
+int[] network       = { 784, 100, 100, 10 };
+var LEARNINGRATE    = 0.0005f;
+var MOMENTUM        = 0.67f;
+var EPOCHS          = 50;
+var BATCHSIZE       = 800;
+var FACTOR          = 0.99f;
 
 RunDemo(d, network, LEARNINGRATE, MOMENTUM, FACTOR, EPOCHS, BATCHSIZE);
 
 print("\nEnd how to build neural networks demo");
 
-static void RunDemo(AutoData d, int[] network, float LR, float MOM, float FACTOR, int EPOCHS, int BATCHSIZE)
+static void RunDemo(AutoData d, int[] NET, float LR, float MOM, float FACTOR, int EPOCHS, int BATCHSIZE)
 {
-    System.Console.WriteLine("NETWORK      = " + string.Join("-", network));
+    System.Console.WriteLine("NETWORK      = " + string.Join("-", NET));
     System.Console.WriteLine("LEARNINGRATE = " + LR);
     System.Console.WriteLine("MOMENTUM     = " + MOM);
     System.Console.WriteLine("BATCHSIZE    = " + BATCHSIZE);
@@ -31,7 +31,7 @@ static void RunDemo(AutoData d, int[] network, float LR, float MOM, float FACTOR
     System.Console.WriteLine("FACTOR       = " + FACTOR + "\n");
 
     System.Console.WriteLine("Start training");
-    var sTime = RunNet(d, new(network), 60000, LR, MOM, FACTOR, EPOCHS, BATCHSIZE);
+    var sTime = RunNet(d, new Net(NET), 60000, LR, MOM, FACTOR, EPOCHS, BATCHSIZE);
 }
 static float RunNet(AutoData d, Net neural, int len, float LR, float MOM, float FACTOR, int EPOCHS, int BATCHSIZE)
 {
